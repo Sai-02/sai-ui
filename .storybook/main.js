fde2b889ea68172ad618ca7ed03d8e1888ed0432
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-01-28 00:43:21 +0530

    rollup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
+  // "stories": [
+  //   "../src/**/*.stories.mdx",
+  //   "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  // ],
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+    "../src/**/*.stories.jsx"
+ ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
