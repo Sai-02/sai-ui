9fbf65ad43733f04adadd113019154d8c71783c2
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-03-20 22:35:12 +0530

    rollup babel plugin added

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import jsx from "rollup-plugin-jsx";
 import postcss from "rollup-plugin-postcss";
+import { babel } from "@rollup/plugin-babel";
 const packageJson = require("./package.json");
 
 export default [
@@ -24,12 +25,24 @@ export default [
       postcss({
         // extensions: [".css"],
       }),
+      babel({
+        babelHelpers: "bundled",
+        presets: ["@babel/preset-react"],
+        exclude: "/node_modules/",
+      }),
+      resolve(),
       jsx({ factory: "React.createElement" }),
       commonjs(),
-      resolve(),
       terser(),
     ],
-    external: ["react", "react-dom", "prop-types"],
+    external: [
+      "react",
+      "react-dom",
+      "prop-types",
+      "@fortawesome/fontawesome-svg-core",
+      "@fortawesome/free-regular-svg-icons",
+      "@fortawesome/free-solid-svg-icons",
+      "@fortawesome/react-fontawesome",
+    ],
   },
-  
 ];
