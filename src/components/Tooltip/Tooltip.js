bc95f732f636f3603970c7bfcf8ba04db6d0465e
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-05-04 23:36:57 +0530

    basic usecases added

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,7 +1,68 @@
 import React from "react";
 import "./Tooltip.css";
-const Tooltip = () => {
-  return <div>Tooltip</div>;
+const Tooltip = ({ children, content, position, xDistance,yDistance }) => {
+  const getToolTipStyles = () => {
+    if (position === "" || position == undefined) return {};
+    switch (position) {
+      case "top":
+        return {
+          top: "-10px",
+        };
+      case "top-left":
+        return {
+          top: "-10px",
+          left: "-24px",
+        };
+      case "top-right":
+        return {
+          top: "-10px",
+          right: "-24px",
+        };
+      case "left":
+        return {
+          left: "calc(-100% - 10px)",
+          top: "50%",
+          transform: "translateY(-50%)",
+        };
+      case "left-top":
+        return {
+          left: "calc(-100% - 10px)",
+          top: "-100%",
+        };
+      case "left-bottom":
+        return {
+          left: "calc(-100% - 10px)",
+        };
+      case "right":
+        return {
+          right: "calc(-100% - 10px)",
+          top: "50%",
+          transform: "translateY(-50%)",
+        };
+      case "right-bottom":
+        return {
+          right: "calc(-100% - 10px)",
+        };
+      case "right-top":
+        return {
+          right: "calc(-100% - 10px)",
+          top: "-100%",
+        };
+    }
+  };
+  return (
+    <span className="sui-tooltip-wrapper">
+      <p className="sui-tooltip-text">{children}</p>
+      <div
+        className="sui-tooltip"
+        style={{
+          ...getToolTipStyles(),
+        }}
+      >
+        {content}
+      </div>
+    </span>
+  );
 };
 
 export default Tooltip;
--- src/stories/Tooltip.stories.js
+++ src/stories/Tooltip.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Tooltip from "../components/Tooltip";
+import "./test.css";
 export default {
   title: "Tooltip",
   component: Tooltip,
@@ -9,9 +10,28 @@ export const Primary = () => (
     className=""
     style={{
       display: "flex",
+      flexDirection: "column",
     }}
   >
-    <Tooltip />
+    <div className="">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis pariatur
+        magni temporibus aperiam molestiae corrupti adipisci magnam numquam ab
+        nemo.
+      </p>
+    </div>
+    <div
+      className=""
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "3rem",
+      }}
+    >
+      <Tooltip content={<>Delete</>} position={"right-bottom"}>
+        Hi hello
+      </Tooltip>
+    </div>
   </div>
 );
 Primary.storyName = "Tooltip";
